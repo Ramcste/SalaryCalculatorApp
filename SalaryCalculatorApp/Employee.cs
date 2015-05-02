a903dabdf8a01ad0ec513d52bf0ec2d4aa2e6e6e
ramcste <lucky.guyram@gmail.com>
2015-05-02 11:20:23 +0600

    Initial Commit

---
+++ SalaryCalculatorApp/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicAmountCalculatorApp
+{
+    class Employee
+    {
+        public string employeeName;
+        public double basicAmount;
+        public double houseRent;
+        public double medicalAllowance;
+        
+
+        public double GetMedicalAllowance(double medicalAllowancePercent) 
+        {
+           medicalAllowance= basicAmount * medicalAllowancePercent/100;
+
+           return medicalAllowance;
+
+        }
+
+
+        public double GetHouseRent(double houseRentPercent)
+        {
+             houseRent=basicAmount * houseRentPercent/100;
+             return houseRent;
+
+        }
+
+        public double GetEmployeeSalary()
+        {
+            return basicAmount + (medicalAllowance + houseRent);
+        }
+
+
+
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using basicAmountCalculatorApp;
+
+namespace SalaryCalculatorApp
+{
+    public partial class SalaryCalculator : Form
+    {
+        public SalaryCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void showMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+
+            employee.employeeName = employeeNameTextBox.Text;
+            employee.basicAmount = double.Parse(employeeBasicAmountTextBox.Text);
+
+            employee.GetHouseRent(double.Parse(houseRentTextBox.Text));
+            employee.GetMedicalAllowance(double.Parse(medicalAllowanceTextBox.Text));
+            MessageBox.Show(employee.employeeName+" ,your salary is: "+employee.GetEmployeeSalary());
+
+
+        }
+    }
+}
